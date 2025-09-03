b8a28c48f5eb0efb11a9c22f34e05516031b76ad
artemgassan <artemgassan@ya.ru>
2025-09-03 15:50:50 +0300

    init: initialized ui-kit

--- apps/chess/src/app/app.ts
+++ apps/chess/src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import {TuiRoot} from '@taiga-ui/core';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, TuiRoot],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected title = 'chess';
-}
+export class App {}
