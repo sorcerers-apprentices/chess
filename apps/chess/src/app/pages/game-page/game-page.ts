4012909d9b4be32b63c2cb8bb8c1935716892e3f
artemgassan <artemgassan@ya.ru>
2025-09-06 04:20:21 +0300

    feat: added navigation block

--- apps/chess/src/app/pages/game-page/game-page.ts
+++ apps/chess/src/app/pages/game-page/game-page.ts
@@ -1,9 +1,11 @@
 import { Header } from '../../components/header/header';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiNavigation } from '@taiga-ui/layout';
+import { Navigation } from '../../components/navigation/navigation';
 
 @Component({
   selector: 'app-game-page',
-  imports: [Header],
+  imports: [Header, TuiNavigation, Navigation],
   templateUrl: './game-page.html',
   styleUrl: './game-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
