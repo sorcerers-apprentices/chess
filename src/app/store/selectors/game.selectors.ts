508f8fba91b6378f0854dae369c474236f95723f
artemgassan <artemgassan@ya.ru>
2025-09-15 18:03:30 +0300

    feat: added a selector for selecting possible moves

--- src/app/store/selectors/game.selectors.ts
+++ src/app/store/selectors/game.selectors.ts
@@ -1,3 +1,4 @@
+import type { Chess } from 'chess.js';
 import { createSelector } from '@ngrx/store';
 import type { AppStateType } from '@/app/store/states/app.state';
 import type { GameStateType } from '@/app/store/states/game.state';
@@ -7,4 +8,8 @@ const selectGame = (state: AppStateType): GameStateType => state.game;
 export const selectChessGame = createSelector(
   selectGame,
   (state: GameStateType) => state.game,
+);
+
+export const selectChessFen = createSelector(selectChessGame, (game: Chess) =>
+  game.fen(),
 );
