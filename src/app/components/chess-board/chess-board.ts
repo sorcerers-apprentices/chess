ad7deb90eaf89a8491d6187667d07ef9d40b1f55
Anna Golosova <golosova76@gmail.com>
2025-09-16 21:57:58 +0300

    fix: change drag&drop

--- src/app/components/chess-board/chess-board.ts
+++ src/app/components/chess-board/chess-board.ts
@@ -29,36 +29,43 @@ import { CdkDropListGroup } from '@angular/cdk/drag-drop';
 })
 export class ChessBoard {
   public readonly orientation = input.required<BoardOrientationType>();
+  // emit event наверх, один раз на «успешный» DnD-ход: { from, to }.
   public readonly move = output<ChessMovePayloadType>();
 
+  // emit event наверх, user начал перетаскивание фигуры с клетки
   public readonly dragStart = output<SquareType>();
+  // emit event наверх, перетаскивание завершено (не важно, был drop или отмена)
   public readonly dragEnd = output<void>();
 
+  // Массив файлов (a…h) для подписи оси X.
   protected readonly files = FILES;
+  // Массив рангов (8…1) для подписи оси Y в «верх-вниз».
   protected readonly ranks = RANKS_TOP_DOWN;
 
   protected readonly boardStateUI = inject(BoardStateService);
 
-  // 🔹 Добавляем локальный сигнал: пока null (поведение как у тебя сейчас)
+  // Добавляем локальный сигнал: пока null
   protected readonly allowedTargets = signal<ReadonlySet<SquareType> | null>(
     null,
   );
 
-  protected readonly allSquareIds = computed(
-    () => this.squares().map((s) => s.square), // ['a1', 'b1', ..., 'h8']
+  //список id клеток в текущей разметке передается в клетку
+  protected readonly allSquareIds = computed(() =>
+    this.squares().map((s) => s.square),
   );
 
-  /** Сигнал с клетками для отрисовки */
+  /** Сигнал с клетками для отрисовки - источник правды для шаблона по клеткам. На вход берёт три сигнала*/
   protected readonly squares = computed<readonly SquareUiStateType[]>(() => {
     const from = this.dragFrom();
     const over = this.dragOver();
-    const allow = this.allowedTargets();
+    const allow = this.allowedTargets(); // разрешённые цели (или null).
 
+    //мапит базовое состояние из boardStateUI.squares() и рассчитывает три флага подсветки
     return this.boardStateUI.squares().map((s) => {
       const isFrom = from === s.square;
       const isOver = over === s.square;
 
-      // 🔹 единое правило: если allow=null → как раньше (разрешено всё, кроме from)
+      // единое правило: если allow=null → как раньше (разрешено всё, кроме from)
       const canDropHere =
         from !== null &&
         s.square !== from &&
@@ -67,12 +74,15 @@ export class ChessBoard {
       const isOverAllowed = isOver && canDropHere;
       const isOverDenied = isOver && !canDropHere;
 
+      // возвращаем расширенный SquareUiStateType с флагами подсветок
       return { ...s, isFrom, isOverAllowed, isOverDenied };
     });
   });
 
   // подсветка DnD
+  //Какая клетка стала источником DnD (когда подняли фигуру). null — ничего не тянем
   private readonly dragFrom = signal<SquareType | null>(null);
+  // Какую клетку сейчас подсвечиваем по ходу DnD.
   private readonly dragOver = signal<SquareType | null>(null);
 
   constructor() {
@@ -98,16 +108,17 @@ export class ChessBoard {
     this.dragEnd.emit();
   }
 
-  protected onSquareMove = (e: ChessMovePayloadType): void => {
+  //клетка-цель сгенерировала итог события «фигура упала ко мне», и прислала payload { from, to }
+  protected onSquareMove = (payload: ChessMovePayloadType): void => {
     // пока лог и сброс подсветки
-    console.log('[ChessBoard] DND move payload', e);
+    console.log('[ChessBoard] DND move payload', payload);
 
     this.dragFrom.set(null);
     this.dragOver.set(null);
 
-    // диспатч хода в BoardStateService
-    this.boardStateUI.movePiece(e);
+    // применяем ход к UI-состоянию (фигуры на доске реально переезжают в сервисе)
+    this.boardStateUI.movePiece(payload);
 
-    this.move.emit(e);
+    this.move.emit(payload);
   };
 }
--- src/app/components/chess-square/chess-square.ts
+++ src/app/components/chess-square/chess-square.ts
@@ -14,6 +14,7 @@ import type {
 
 import { PIECE_ICON_URL } from '@/app/constants/chess-piece.constans';
 import type { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { CdkDragPlaceholder } from '@angular/cdk/drag-drop';
 import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
 import type {
   ChessMovePayloadType,
@@ -24,56 +25,77 @@ import { isDragData, isDropData } from '@/app/utilities/chess-piece';
 
 @Component({
   selector: 'app-chess-square',
-  imports: [CdkDropList, CdkDrag],
+  imports: [CdkDropList, CdkDrag, CdkDragPlaceholder],
   templateUrl: './chess-square.html',
   styleUrl: './chess-square.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChessSquare {
+  // координата клетки (id)
   public readonly square = input.required<SquareType>();
+  //фон клетки
   public readonly backgroundColor = input.required<SquareColorType>();
+  // объект фигуры в клетке (id, цвет, что за фигура)
   public readonly piece = input<PieceType | null>(null);
+  //event попытка хода drop: { from, to }
   public readonly chessMove = output<ChessMovePayloadType>();
 
   // флаги подсветки — придут от родителя
+  // источник перетягивания
   public readonly isFrom = input<boolean>(false);
+  //курсор/фигура «над этой клеткой, ход разрешён».
   public readonly isOverAllowed = input<boolean>(false);
+  // курсор/фигура «над этой клеткой, ход запрещён».
   public readonly isOverDenied = input<boolean>(false);
+
   // добавляем события для родителя
+  // старт с этой клетки а4 h5
   public readonly dragStartSquare = output<SquareType>();
+  // перешли на эту клетку
   public readonly dragEnterSquare = output<SquareType>();
+  // поставили (успешно или отменили)
   public readonly dragEndSquare = output<void>();
 
+  // Список всех клеток доски (координат) чтобы сконфигурировать «с кем связана» текущая cdkDropList (клетка)
   public readonly allSquares = input.required<readonly SquareType[]>();
 
+  // От фигуры → к URL иконки: PIECE_ICON_URL[kind][color] или пустая строка, если фигуры нет. Идёт в <img [src]>
   protected readonly icon: Signal<string> = computed(() => {
-    const p = this.piece();
-    return p ? PIECE_ICON_URL[p.kind][p.color] : '';
+    const pieceIcon = this.piece();
+    return pieceIcon ? PIECE_ICON_URL[pieceIcon.kind][pieceIcon.color] : '';
   });
 
+  //Массив «подключённых» DropList (все клетки кроме себя). Используется в шаблоне на cdkDropListConnectedTo
+  //Это позволяет ввозить/вывозить drag в любую другую клетку.
   protected readonly connectedToIds = computed(() =>
     this.allSquares().filter((id) => id !== this.square()),
   );
 
+  // Альт-текст для иконки фигуры
   protected readonly altText = computed(() => {
-    const p = this.piece();
-    return p ? `${p.color} ${p.kind}` : '';
+    const pieceAltText = this.piece();
+    return pieceAltText ? `${pieceAltText.color} ${pieceAltText.kind}` : '';
   });
 
+  // Флаг «в клетке есть фигура»
   protected readonly hasPiece = computed(() => this.piece() !== null);
 
+  // Данные, которые CDK будет «таскать»: { piece, fromSquare } или null, если нечего тащить.
+  //DragDataType Что тащим (источник)
   protected readonly dragData: Signal<DragDataType | null> =
     computed<DragDataType | null>(() => {
       const pieceDrag = this.piece();
       return pieceDrag ? { piece: pieceDrag, fromSquare: this.square() } : null;
     });
 
+  // Данные целевой DropList: куда бросаем (клетка square) и кто там сейчас лежит (piece | null).
+  // DropDataType Куда бросаем (цель)
   protected readonly dropData = computed<DropDataType>(() => ({
     square: this.square(),
     piece: this.piece(),
   }));
 
-  // тонкие хендлеры без логики
+  // хендлеры без логики
   public handleDragStart(): void {
     console.log('[ChessSquare] dragStart', this.square());
     this.dragStartSquare.emit(this.square());
@@ -87,6 +109,7 @@ export class ChessSquare {
     this.dragEndSquare.emit();
   }
 
+  // функция-предикат для cdkDropListEnterPredicate. Она решает: пускать ли текущий Drag в эту DropList.
   public readonly canEnter = (
     drag: CdkDrag<DragDataType>,
     drop: CdkDropList<DropDataType>,
