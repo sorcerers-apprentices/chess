4597245f46c6dc3d99c95f6c373812e8da01818f
daryakolenchenko <daria.kolenchenko@gmail.com>
2025-09-18 22:55:43 +0300

    fix: fix horizontal scroll and translate confirmation

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -4,6 +4,7 @@ import { LOCAL_STORAGE_KEY } from '../constants/auth.constants';
 import type { Observable } from 'rxjs';
 import { TUI_CONFIRM, type TuiConfirmData } from '@taiga-ui/kit';
 import { TuiResponsiveDialogService } from '@taiga-ui/addon-mobile';
+import { TranslateService } from '@ngx-translate/core';
 
 export const authenticatedGuardFunction: CanActivateFn = () => {
   const router = inject(Router);
@@ -19,11 +20,12 @@ export const anonymousGuardFunction: CanActivateFn = () => {
 };
 
 export const canDeactivatePopup = (): Observable<boolean> => {
+  const translateService = inject(TranslateService);
   const dialogs = inject(TuiResponsiveDialogService);
   const data: TuiConfirmData = {
-    content: 'Are you sure you want to leave your game?',
-    yes: 'Yes',
-    no: 'No',
+    content: translateService.instant('game.notification'),
+    yes: translateService.instant('game.yes'),
+    no: translateService.instant('game.no'),
   };
 
   return dialogs.open<boolean>(TUI_CONFIRM, {
