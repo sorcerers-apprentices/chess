f5fb5affed569e8cec37913eaa27a437628e1460
Anna Golosova <golosova76@gmail.com>
2025-09-22 18:06:55 +0300

    feat: add UI game settings panel

--- src/app/components/game-settings/game-settings.ts
+++ src/app/components/game-settings/game-settings.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiButton, TuiIcon, TuiScrollbar } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-game-settings',
-  imports: [],
+  imports: [TuiButton, TuiIcon, TuiScrollbar],
   templateUrl: './game-settings.html',
   styleUrl: './game-settings.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
