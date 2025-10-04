15db309b738cff9eb4bfec2d76c4888b29ffc64c
Anna Golosova <golosova76@gmail.com>
2025-10-04 18:42:44 +0300

    fix: change jest config

--- jest.config.ts
+++ jest.config.ts
@@ -19,8 +19,13 @@ const config: Config = {
     ],
   },
   transformIgnorePatterns: [
-    //'node_modules/(?!(?:@angular|@angular/cdk|rxjs|tslib|@supabase|@ngrx|@taiga-ui|@angular-devkit|@ngtools|@ng-web-apis)/)'
     'node_modules/(?!(?:@angular|@angular/cdk|rxjs|tslib|@supabase|@ngrx|@taiga-ui|@angular-devkit|@ngtools|@ng-web-apis|@maskito|@ngx-translate)/)',
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/e2e/',
+    'app.routes.ts',
   ],
 
   moduleNameMapper: {
